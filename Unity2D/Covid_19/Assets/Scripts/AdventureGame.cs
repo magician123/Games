7d38adede0f64477f441bbc3fe493379a1cb7798
Tshepiso <timarape.gmail.com>
2020-05-17 09:14:44 +0200

    Initial Commit

---
+++ Unity2D/Covid_19/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+
+    State state;
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+
+    } 
+
+    
+    void Update()
+    {
+
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+
+        }
+
+        textComponent.text = state.GetStateStory();
+        
+    }
+}
---
+++ Unity2D/FirstProject/Assets/FirstProject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstProject : MonoBehaviour
+{   
+    int maxNumber;
+    int minNumber;
+    int guess;
+    void Start()
+    {
+        StartGame();
+    
+    }
+
+    private void StartGame()
+    {
+        maxNumber = 1000;
+        minNumber = 1;
+        guess = 500; 
+
+        Debug.Log("Welcome to number wizard");
+        Debug.Log("Pick a number");
+        Debug.Log($"The highest number is {maxNumber} ");
+        Debug.Log($"The lowest number is {minNumber} ");
+        Debug.Log("Is the number higher or lower than 500?");
+        Debug.Log("Press Up = Higher, Press Down = Lower, Press Enter= Correct");
+
+        maxNumber += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            minNumber = guess;
+            guess = GuessedNumer(minNumber,maxNumber);
+
+            Debug.Log($"Is higher or lower than {guess}");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+  
+            maxNumber = guess;
+            guess = GuessedNumer(minNumber, maxNumber);
+
+            Debug.Log($"Is higher or lower than {guess}");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("The Enter Key Ws pressed");
+            StartGame();
+        }
+       
+    }
+
+    int GuessedNumer(int min, int max)
+    {
+        return (min + max) / 2;
+    }
+}
